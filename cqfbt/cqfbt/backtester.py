41d33a1de4bfd73bdcad550ae71e6795dbdfa77d
Jack Young <59127086+JackRYoung@users.noreply.github.com>
2023-01-31 16:27:15 -0500

    data structure edits

--- cqfbt/cqfbt/backtester.py
+++ cqfbt/cqfbt/backtester.py
@@ -2,6 +2,7 @@
 
 import yfinance as yf
 import datetime as dt
+import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -25,17 +26,17 @@ class engine:
     def __init__(self, ptfl=[], start_date='', end_date='', interval='', init_capital=5e4):
         self.capital = init_capital
         self.portfolio_assets = ptfl
-        self.portfolio_allocations = np.zeros(len(ptfl))
-        self.get_info_on_stock(start_date, end_date, interval)
+        self.portfolio_allocations = np.zeros((len(ptfl), 1))
+        self.get_info_on_stocks(start_date, end_date, interval)
 
         # for now, we are restrained in the sense that all assets must
         # have the exact same length of data
         # unfortunately this means cryptocurrencies cannot be in the
-        # portfolio while regular stocks are, I will work on this for version 0.0.2
+        # portfolio while regular stocks are
         if len(ptfl) != 0:
             length = len(pd.read_csv("cqfbt\\data\\"+ptfl[0]+"_hist.csv"))
             self.arr_length = length
-            self.arr = np.ndarray((self.arr_length, len(ptfl), 7))
+            self.arr = np.zeros((self.arr_length, len(ptfl), 7))
 
         idx = 0
         for i in range(0, len(ptfl)):
@@ -48,13 +49,13 @@ class engine:
                 self.arr_length = len(self.dates)
             idx = 1
 
-        self.portfolio_history = np.zeros(
-            (self.arr_length, len(self.portfolio_assets)+2))
+        self.portfolio_history = np.ndarray(
+            (self.arr_length, len(self.portfolio_assets)+2, 1))
 
     # Gets yf data on stocks in optional portfolio argument
     # Saves csv files in data folder
 
-    def get_info_on_stock(self, start, end, interval):
+    def get_info_on_stocks(self, start, end, interval):
         for i in range(0, len(self.portfolio_assets)):
             ticker = self.portfolio_assets[i]
             stock = yf.Ticker(ticker)
@@ -74,6 +75,7 @@ class engine:
     # Callable by user
     def add_strategy(self, strategy):
         self.strategies.append(strategy)
+        # Exend portfolio_history into 3rd dimension +1
 
     # Returns the next time-slice of data
     # If an error was thrown due to failure to execute orders,
@@ -87,9 +89,10 @@ class engine:
 
     # Updates portfolio and capital according to executed orders
     def update_portfolio(self, portfolio_delta, capital_delta):
-        self.capital += capital_delta
+        self.capital = round(self.capital + capital_delta, 2)
         for i in range(0, len(self.portfolio_assets)):
-            self.portfolio_allocations[i] += portfolio_delta[i]
+            self.portfolio_allocations[i, 0] = round(
+                self.portfolio_allocations[i, 0] + portfolio_delta[i, 0], 2)
 
     # TODO: Executes orders at prices found in the data, and returns change in
     #   portfolio and capital as a result. Returns any orders which did not execute
@@ -102,7 +105,7 @@ class engine:
         # standard deviation derived from other data
         outstanding_orders = None
         capital_delta = 0
-        portfolio_delta = np.zeros(len(self.portfolio_assets))
+        portfolio_delta = np.ndarray((len(self.portfolio_assets), 1))
 
         if self.capital+capital_delta < 0:
             raise InsufficientFundsException()
@@ -116,17 +119,18 @@ class engine:
             date, data = self.next_data(error)
             # Portfolio History keeps track of asset allocations
             for j in range(0, len(self.portfolio_assets)):
-                self.portfolio_history[i, j] = self.portfolio_allocations[j]
+                self.portfolio_history[i, j,
+                                       0] = self.portfolio_allocations[j, 0]
             # And capital
             self.portfolio_history[i, len(
-                self.portfolio_assets)] = self.capital
+                self.portfolio_assets), 0] = self.capital
             # And total portfolio value
             self.portfolio_history[i, len(
-                self.portfolio_assets)+1] = self.get_portfolio_cash_value(data)
+                self.portfolio_assets)+1, 0] = self.get_portfolio_cash_value(data)
             # Testing purposes
             print(str(i) + ' ::: ' + date + ' ::: ' +
-                  list_to_str(self.portfolio_history[i, :]))
-            # for now supports one strategy   V
+                  list_to_str(self.portfolio_history[i, :, 0]))
+            # for now supports only one   V   strategy
             self.orders = self.strategies[0].execute(
                 date, data, self.portfolio_allocations, self.capital, self.orders, error)
             error = False
